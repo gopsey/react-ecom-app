fc6cee79bebcaaff536dc718bbd3796e5b1c2f68
Gopalakrishnan <gopalkris96@gmail.com>
2024-07-11 16:35:42 +0530

    import error resolved

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { API_BASE_URL } from '../config/config'
 const httpService = axios.create({
    baseURL: process.env.REACT_APP_API_BASE_URL,
    headers: { 'Content-Type': 'application/json' }
